449590254dc77842dd265c9bb784419356242a7b
Winsor <144194276+winsorc@users.noreply.github.com.>
2023-10-21 04:28:57 -0500

    Adds function to count increase per sliding window

--- Day1/depth_increases.py
+++ Day1/depth_increases.py
@@ -3,6 +3,7 @@
 def main():
   numbers = readInput() #assign input list to variable
   print(f"Depth Increases", {depth_increases(numbers)}, "times.")
+  print(f"Depth Window Increases", {depth_windows(numbers)}, "times")
 
 #Defines function to open input file
 def readInput():
@@ -17,14 +18,32 @@ def depth_increases(numbers):
   numbers = readInput() #assign input list to variable
   prev_num = numbers[0] #initialize previous number position for comparing increases
   count = 0 #initalizes variable to count number of increases
-#iterate through each number in input, excluding first number
+#Iterate through each number in input, excluding first number
   for num in numbers[1:]:
     if num > prev_num: #checks if current number is larger than previous number
       count +=1 #increments count
     prev_num = num #Moves current number to previous number
     
   return count
-  
+#Defines function to compare depths in sliding window
+def depth_windows (numbers):
+  numbers = readInput() 
+  window_size = 3 
+  count  = 0
+  total_window=[] #Defines new list to store window depth increases
+  for i in range(len(numbers) - window_size + 1): #Iterates input and keeps last number of previous window
+    total_window.append((sum(numbers[i: i + window_size]))) #Appends the sum of each window to a new list
+  for x in range (1, len(total_window)): 
+    if total_window[x - 1] < total_window[x]: #Checks if previous window is smaller
+      count+=1 #Counts an increase
+  return count  
+    
+      
+    
+    
+     
+     
+
   
 
 if __name__ == "__main__":
